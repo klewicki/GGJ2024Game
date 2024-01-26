0b30a9d5ab18079ce569695decce058e65339e05
klewicki <kl.krzysztof.lewicki@gmail.com>
2024-01-26 19:02:38 +0100

    Simple main menu set up as start scene with buttons for server/client selection

---
+++ GodotProject/ServerClientSelector.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func ServerButtonClicked():
+	print("server button clicked!")
+	#TODO: do logic
+	#TODO: hide panel
+	return
+	
+func ClientButtonClicked():
+	print("client button clicked!")
+	#TODO: do logic
+	#TODO: hide panel
+	return
